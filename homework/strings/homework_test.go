7f6ad9629e3e031b4fd72e302d859631fb278e8a
hazard <hazard.pro@gmail.com>
2025-07-12 20:11:33 +0300

    hw3 implemented

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -11,27 +11,46 @@ import (
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	return COWBuffer{
+		data: data,
+		refs: new(int),
+	}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	*b.refs++
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	b.refs = nil
+	b.data = nil
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if index < 0 || index > len(b.data)-1 {
+		return false
+	}
+
+	if *b.refs > 0 {
+		temp := make([]byte, len(b.data))
+		copy(temp, b.data)
+		temp[index] = value
+		b.data = temp
+		*b.refs = 0
+	}
+
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	return unsafe.String(&b.data[0], len(b.data))
 }
 
 func TestCOWBuffer(t *testing.T) {
